d9fe21ba1209718f16a710e33c912ec7e0db4f5c
Anupriya Dayaratne <38352142+Anupriya-Dayaratne@users.noreply.github.com>
2019-11-01 23:15:16 +1300

    First_Version_startup_project
    Bootcamp

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Anupriya Dayaratne on 20/10/19.
+//  Copyright © 2019 Anupriya Dayaratne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I AM RICH/I AM RICHTests/I_AM_RICHTests.swift
@@ -0,0 +1,34 @@
+//
+//  I_AM_RICHTests.swift
+//  I AM RICHTests
+//
+//  Created by Anupriya Dayaratne on 20/10/19.
+//  Copyright © 2019 Anupriya Dayaratne. All rights reserved.
+//
+
+import XCTest
+@testable import I_AM_RICH
+
+class I_AM_RICHTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
